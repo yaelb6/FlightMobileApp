fa7d6c13dc8fa8b07699c62be5091656246f0276
yaelb6 <yaelberman6@gmail.com>
2020-06-16 16:27:59 +0300

    Add project files.

---
+++ FlightMobileApp/Command.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace FlightMobileApp
+{
+    public class Command
+    {
+        [JsonPropertyName("aileron")]
+        [JsonProperty("aileron")]
+        public double Aileron { get; set; }
+
+        [JsonPropertyName("rudder")]
+        [JsonProperty("rudder")]
+        public double Rudder { get; set; }
+
+        [JsonPropertyName("throttle")]
+        [JsonProperty("throttle")]
+        public double Throttle { get; set; }
+
+        [JsonPropertyName("elevator")]
+        [JsonProperty("elevator")]
+        public double Elevator { get; set; }
+
+        public Command(double Aileron1, double Rudder1, double Throttle1, double Elevator1)
+        {
+            Aileron = Aileron1;
+            Rudder = Rudder1;
+            Throttle = Throttle1;
+            Elevator = Elevator1;
+        }
+    }
+}
