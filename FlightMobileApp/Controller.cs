4e992d9029a31046c00c5452f36442eb491c5e4d
JennyDavidov <48158720+JennyDavidov@users.noreply.github.com>
2020-06-16 17:08:02 +0300

    jenny

---
+++ FlightMobileApp/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightMobileApp
+{
+    public class Controller
+    {
+        SimulactorClient telnetClient;
+        private Command c;
+        public Controller(SimulactorClient telnetClient)
+        {
+            this.telnetClient = telnetClient;
+            //stop = false;
+        }
+
+        public void Connect()
+        {
+            telnetClient.Connect();
+        }
+        public void Disconnect()
+        {
+            //stop = true;
+            telnetClient.Disconnect();
+        }
+
+
+
+    }
+}
